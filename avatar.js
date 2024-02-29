349d79fb7e830ac9c02bb03e53d7a5972aa432f7
Xayida83 <lotta.lindberg83@gmail.com>
2024-02-29 10:03:39 +0100

    mobile version in todo

--- avatar.js
+++ avatar.js
@@ -19,34 +19,70 @@
 //     console.log(data);
 //   });
 
-const url = `https://type.fit/api/quotes`;
+// const url = `https://type.fit/api/quotes`;
+// const url = 'https://zenquotes.io/api/random/';
 
-  let getData = async (url) => {
-    let response = await fetch(url); 
-    let json = await response.json();
-    console.log(json);
+  // let getData = async (url) => {
+  //   let response = await fetch(url); 
+  //   let json = await response.json();
+  //   console.log(json);
     
-    return json;   
-  }
+  //   return json;   
+  // }
   
 
-  let getRandomQuote = async(url) => {
-    let json = await getData(url);
-    let randomIndex = Math.floor(Math.random() * json.length);
-    let randomQuote = json[randomIndex];
-    console.log(randomQuote);
+  // let getRandomQuote = async(url) => {
+  //   let json = await getData(url);
+  //   let randomIndex = Math.floor(Math.random() * json.length);
+  //   let randomQuote = json[randomIndex];
+  //   console.log(randomQuote);
 
-    // Dela strängen vid kommatecknet och behåll den första delen
-    randomQuote.author = randomQuote.author.split(',')[0];
+  //   // Dela strängen vid kommatecknet och behåll den första delen
+  //   randomQuote.author = randomQuote.author.split(',')[0];
 
-    return randomQuote;    
-  }
+  //   return randomQuote;    
+  // }
 
-  let displayQuote = async () => {
-    let randomQuote = await getRandomQuote(url);
+  // let displayQuote = async () => {
+  //   let randomQuote = await getRandomQuote(url);
 
-    document.getElementById('quote').innerText = randomQuote.text;
-    document.getElementById('author').innerText = randomQuote.author;
-  }
+  //   document.getElementById('quote').innerText = randomQuote.text;
+  //   document.getElementById('author').innerText = randomQuote.author;
+  // }
 
-  displayQuote();
+  // displayQuote();
+
+  // Generellt API-anrop
+let getData = async (url) => {
+    let response = await fetch (url);
+    let dataFetch = await response.json();
+
+    return dataFetch; 
+}
+
+let createProfileCard = async (params = {}) => {
+    let character = await getData(`https://zenquotes.io/api/quotes/${params}`); 
+
+    character.results.forEach (profile => {
+
+    const { q, a } = profile; 
+
+    document.getElementById('quote').innerText = profile.q;
+    document.getElementById('author').innerText = profile.a;
+    console.log(character);
+    // let div = document.createElement("div");
+    // div.style.border = "2px solid peru"; 
+    // div.innerHTML = 
+    // <p>Name: ${name}</p>
+    // <p>Gender: ${gender}</p>
+    // <p>Species: ${species}</p>
+    // <p>Status: ${status}</p> 
+    // <img src="${image}"/>
+    
+
+    // document.body.append(div);
+
+});
+
+}
+createProfileCard(params);
